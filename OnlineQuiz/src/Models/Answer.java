3fe20a855ef77815ef4daab937bda3e607779b8d
Annie-PC <Annie-PC@192.168.76.145>
2016-06-29 20:57:45 +0400

    answermodel

--- OnlineQuiz/src/Models/Answer.java
+++ OnlineQuiz/src/Models/Answer.java
@@ -1,5 +1,36 @@
 package Models;
 
+import Models.Question.type;
+
 public class Answer {
+	private int id;
+	private Question question;
+	private String answer;
+	private boolean answerType;
+	
+	public int GetID(){
+		return id;
+	}
+	public void SetID(int id){
+		this.id = id;
+	}
+	public Question GetQuestion(){
+		return question;
+	}
+	public void SetQuestion(Question question){
+		this.question = question;
+	}
+	public String GetAnswer(){
+		return answer;
+	}
+	public void SetAnswer(String answer){
+		this.answer = answer;
+	}
+	public boolean GetQuestionType(){
+		return this.answerType;
+	}
+	public void SetQuestionType(boolean answerType){
+		this.answerType = answerType;
+	}
 	
 }
