d3d9306c81c0d60b63f4fac5e8e77338a7ab9ebf
Guka-PC <Guka-PC@DESKTOP-5VINR2H>
2016-06-29 21:25:39 +0400

    IQuestionRepository

---
+++ OnlineQuiz/src/Interfaces/IAnswerRepository.java
@@ -0,0 +1,20 @@
+package Interfaces;
+
+import java.util.List;
+
+import Models.Answer;
+import Models.Question;
+
+public interface IAnswerRepository {
+	public Answer Get(int id);
+	public List<Answer> GetAll();
+	public List<Answer> GetAll(int questionID);
+	public List<Answer> GetAll(Question question);
+	
+	public boolean Delete(int id);
+	
+	public boolean Update(Answer answer);
+	
+	public void Save(Answer answer);
+	public void SaveRange(List<Answer> answers);
+}
