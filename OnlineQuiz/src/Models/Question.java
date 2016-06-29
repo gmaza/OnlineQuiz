7adee725294e84fccc75ffe44df46bf208f537a5
Annie-PC <Annie-PC@192.168.76.145>
2016-06-29 20:27:07 +0400

    questionModel

--- OnlineQuiz/src/Models/Question.java
+++ OnlineQuiz/src/Models/Question.java
@@ -1,5 +1,41 @@
 package Models;
 
 public class Question {
-
+	public enum type {
+		QuestionResponse, FillInTheBlank, MultipleChoice, 
+		PictureResponse, MultipleChoiceWithMultipleAnswers
+	}
+	
+	private int id;
+	private Quiz quiz;
+	private String question;
+	private type questionType;
+	
+	public int GetID(){
+		return id;
+	}
+	public void SetID(int id){
+		this.id = id;
+	}
+	public Quiz GetQuiz(){
+		return quiz;
+	}
+	public void SetQuiz(Quiz quiz){
+		this.quiz = quiz;
+	}
+	public String GetQuestion(){
+		return question;
+	}
+	public void SetQuestion(String question){
+		this.question = question;
+	}
+	public type GetQuestionType(){
+		return this.questionType;
+	}
+	public void SetQuestionType(type questionType){
+		this.questionType = questionType;
+	}
+	public void SetQuestionType(int ind){
+		this.questionType = type.values()[ind];
+	}
 }
