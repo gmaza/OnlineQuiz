b611f72ab14197ab0f063ea887f06b96dca3bf90
Guka-PC <Guka-PC@DESKTOP-5VINR2H>
2016-06-30 01:17:38 +0400

    UnitOfWork სრულად

--- OnlineQuiz/src/Repository/UnitOfWork.java
+++ OnlineQuiz/src/Repository/UnitOfWork.java
@@ -13,6 +13,26 @@ public class UnitOfWork {
 		_users=new UserRepository();
 	}
 	
+	public IAnswerRepository GetAnswers(){
+		return _answers;
+	}
+	
+	public IMessageRepository GetMessages(){
+		return _messages;
+	}
+	public IQuestionRepository GetQuestions(){
+		return _questions;
+	}
+	public IQuizRepository GetQuizes(){
+		return _quizes;
+	}
+	public IResultRepository GetResults(){
+		return _results;
+	}
+	public IUserRepository GetUsers(){
+		return _users;
+	}	
+	
 	private IAnswerRepository _answers;
 	private IMessageRepository _messages;
 	private IQuestionRepository _questions;
