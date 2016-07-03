64c55594f0b51c81de3d4c946fe1196d8bc5ee29
Guka-PC <Guka-PC@DESKTOP-5VINR2H>
2016-07-03 14:59:00 +0400

    create quiz servlet naxevarze meti

--- OnlineQuiz/src/servlets/CreateQuiz.java
+++ OnlineQuiz/src/servlets/CreateQuiz.java
@@ -3,9 +3,13 @@ package servlets;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Models.Quiz;
+import Repository.UnitOfWork;
 
 /**
  * Servlet implementation class CreateQuiz
@@ -34,8 +38,33 @@ public class CreateQuiz extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		UnitOfWork uwork = new UnitOfWork();
+		
+		
+		String username = "";
+		String loginToken = "";
+		Cookie cookie = null;
+		Cookie[] cookies = null;
+		cookies = request.getCookies();
+		if (cookies != null) {
+			for (int i = 0; i < cookies.length; i++) {
+				cookie = cookies[i];
+				if (cookie.getName().equals("username"))
+					username = cookie.getValue();
+				else if (cookie.getName().equals("loginToken"))
+					loginToken = cookie.getValue();
+			}
+		}
+		
+		String name = request.getParameter("name");
+		
+		Quiz quiz = new Quiz();
+		quiz.SetName(name);
+		quiz.SetAuthor(uwork.GetUsers().Get(username));
+		
+		uwork.GetQuizes().Save(quiz);
+		
+		
 	}
 
 }
